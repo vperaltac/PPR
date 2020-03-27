e524614ec56a59d6d79975c15c0079ab6f46ea4a
Victor Peralta <victorperalta.correo@gmail.com>
2020-03-27 14:09:35 +0100

    corrige bug con la reserva de memoria de extern __shared__

--- ejercicios_cuda/ejercicio8.cu
+++ ejercicios_cuda/ejercicio8.cu
@@ -1,17 +1,14 @@
 #include "stdio.h"
 #include "iostream"
 
-#define  Bsize_addition 256
-#define Bsize_minimum   128
-
-const int N=64;
+const int N=32;
 
 __global__ void reduceSum(float *d_V, int N){
     extern __shared__ float sdata[];
     int tid = threadIdx.x;
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     
-    sdata[tid] = ((i<N) ? d_V[i] : 0.0f);
+	sdata[tid] = ((i < N) ? d_V[i] : 0.f);
     __syncthreads();
 
     for (int s=blockDim.x/2;s>0;s>>=1){
@@ -21,42 +18,30 @@ __global__ void reduceSum(float *d_V, int N){
         __syncthreads();
     }
 
-    if (tid == 0) 
+    if (tid == 0)
         d_V[blockIdx.x] = sdata[0];
+        
 }
 
 int main(){
+    float *a;
+    float *a_d;
     int i;
 
-    dim3 dimBlock(Bsize_addition);
-    dim3 dimGrid ( ceil((float(N)/(float)dimBlock.x)) );
+    a = (float*) malloc(N*sizeof(float));
+    cudaMalloc ((void **) &a_d, sizeof(float)*N);
 
-    dim3 threadsPerBlock(Bsize_minimum, 1);
-    dim3 numBlocks( ceil ((float)(N)/threadsPerBlock.x), 1);
-
-    float * vmin;
-    vmin = (float*) malloc(N*sizeof(float));
-
-    /* Initialize arrays a and b */
-    for (i=0; i<N;i++){
-        vmin[i]= (float) i;
-    }
-
-    float *vmin_d; 
-    cudaMalloc ((void **) &vmin_d, sizeof(float)*N);
-    int smemSize = threadsPerBlock.x*sizeof(float);
-
-    // Kernel launch to compute Minimum Vector
-    reduceSum<<<numBlocks, threadsPerBlock, smemSize>>>(vmin_d, N);
-
-    /* Copy data from device memory to host memory */
-    cudaMemcpy(vmin, vmin_d, numBlocks.x*sizeof(float),cudaMemcpyDeviceToHost);
-
-    /* Print c */
     for (i=0; i<N;i++)
-        printf(" vmin[%d]=%f\n",i,vmin[i]);
+        a[i]= (float) i;
 
-    /* Free the memory */
-    free(vmin);
-    cudaFree(vmin_d);
+    cudaMemcpy(a_d, a, sizeof(float)*N, cudaMemcpyHostToDevice);
+
+    reduceSum<<< 1, N,sizeof(float)*N>>>(a_d,N);
+
+    cudaMemcpy(a, a_d, sizeof(float)*N, cudaMemcpyDeviceToHost);
+
+    printf("suma con reducción: %f\n",a[0]);
+
+    free(a);
+    cudaFree(a_d);
 }
