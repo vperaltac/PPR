63b7634c6e08eafb4c9cb66c93ac8d1bd3720aa7
Victor Peralta <victorperalta.correo@gmail.com>
2020-04-05 16:00:00 +0200

    reductionv3 ya no es necesario

--- p1/floyd/floyd.cu
+++ p1/floyd/floyd.cu
@@ -78,42 +78,6 @@ __global__ void reduce2(T *g_idata, T *g_odata, unsigned int n) {
 }
 
 
-/*
-    This version uses n/2 threads --
-    it performs the first level of reduction when reading from global memory.
-*/
-template <class T>
-__global__ void reduce3(T *g_idata, T *g_odata, unsigned int n) {
-  // Handle to thread block group
-  cg::thread_block cta = cg::this_thread_block();
-  T *sdata = SharedMemory<T>();
-
-  // perform first level of reduction,
-  // reading from global memory, writing to shared memory
-  unsigned int tid = threadIdx.x;
-  unsigned int i = blockIdx.x * (blockDim.x * 2) + threadIdx.x;
-
-  T mySum = (i < n) ? g_idata[i] : 0;
-
-  if (i + blockDim.x < n) mySum += g_idata[i + blockDim.x];
-
-  sdata[tid] = mySum;
-  cg::sync(cta);
-
-  // do reduction in shared mem
-  for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
-    if (tid < s) {
-      sdata[tid] = mySum = mySum + sdata[tid + s];
-    }
-
-    cg::sync(cta);
-  }
-
-  // write result for this block to global mem
-  if (tid == 0) g_odata[blockIdx.x] = mySum;
-}
-
-
 __global__ void floyd_kernel2D(int *M, const int nverts, const int k){
 	int j = blockIdx.x * blockDim.x + threadIdx.x;
 	int i = blockIdx.y * blockDim.y + threadIdx.y;
